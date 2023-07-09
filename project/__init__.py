4d49a47bbc8d6e1f9de519c63c4de8aebb34f1a8
Victor Alteirac <v.alteirac@geofit-expert.fr>
2023-07-09 15:50:11 +0200

    add forum part

--- project/__init__.py
+++ project/__init__.py
@@ -1,9 +1,10 @@
+import datetime
 from pathlib import Path
-
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
@@ -20,18 +21,34 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from .models import User
-
+    from .models import User, Post
+    # needed by Flask LoginManager()
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
     # create database
-    from . import models
     if not Path('./instance/db.sqlite').exists():
         with app.app_context():
             db.create_all()
+            # add admin user
+            admin = User(
+                email='admin@gmail.com', 
+                name='admin', 
+                password=generate_password_hash("admin", method='scrypt')
+            )
+            db.session.add(admin)
+            db.session.commit()
+            # add admin post
+            admin_post = Post(
+                user_id=admin.id,
+                title="admin title",
+                content="this is admin post",
+                timestamp=datetime.datetime.now()
+            )
+            db.session.add(admin_post)
+            db.session.commit()
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
--- project/main.py
+++ project/main.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+import datetime
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+
 from . import db
+from .models import Post, User
+from .utils import get_post
 
 main = Blueprint('main', __name__)
 
@@ -11,4 +15,74 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    return render_template('profile.html', name=current_user.name)
+    nb_posts = db.session.query(Post).filter(Post.user_id == current_user.id).count()
+    posts = db.session.query(Post).filter(Post.user_id == current_user.id).all()
+    return render_template(
+        'profile.html',
+        name=current_user.name, 
+        nb_posts=nb_posts, 
+        posts=posts)
+
+@main.route('/blog')
+@login_required
+def blog():
+    # join Post and User database
+    join_db = db.session.query(Post, User).join(User, User.id == Post.user_id).all()
+    return render_template(
+        'blog.html',
+        join_db=join_db)
+
+@main.route('/blog/<int:post_id>')
+@login_required
+def post(post_id):
+    return render_template('post.html', post=get_post(post_id))
+
+@main.route('/blog/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            new_post = Post(
+                user_id=current_user.id,
+                title=title,
+                content=content,
+                timestamp=datetime.datetime.now()
+            )
+            db.session.add(new_post)
+            db.session.commit()
+           
+            return redirect(url_for('main.profile'))
+
+    return render_template('create.html')
+
+@main.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            db.session.query(Post).filter(Post.id == id).update(
+                {'title':title,
+                 'content':content}
+            )
+            db.session.commit()
+            return redirect(url_for('main.profile'))
+
+    return render_template('edit.html', post=post)
+
+@main.route('/blog/<int:id>/edit', methods=('GET', 'POST'))
+def delete(id):
+    post = get_post(id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('"{}" was successfully deleted!'.format(post.title))
+    return redirect(url_for('main.profile'))
